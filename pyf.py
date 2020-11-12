9152930636b4584c1b7ed5dc62ff6577cf7d7057
Julangar <jagarciaguer@msn.com>
2020-11-12 13:27:44 -0500

    Cifras

--- pyf.py
+++ pyf.py
@@ -1,6 +1,6 @@
 from random import randint
 
-def generar_secreto(cantidad):
+def generar_secreto(cantidad): #Se genera un numero aleatorio sin repeticion
     secreto = []
     while True:
         d = randint(0, 9)
@@ -10,7 +10,7 @@ def generar_secreto(cantidad):
             break
     return secreto
 
-def validar_numero(numero):
+def validar_numero(numero): # Se valida que cada digito del numero sea diferente
     if len(numero) == 1:
         return True
     else:
@@ -19,13 +19,13 @@ def validar_numero(numero):
         else: 
             return validar_numero(numero[1:])
 
-def validar_longitud(numero,longitud):
+def validar_longitud(numero,longitud): #Se revisa la longitud del numero
     if len(numero) == longitud:
         return validar_numero(numero)
     else:
         return False
 
-def comprobar(intentos,numero,entrada):
+def comprobar(intentos,numero,entrada): #Comienza el juego de picas y fijas
     resultado = ""
     for i in range(intentos):
             fijas = 0
@@ -33,7 +33,8 @@ def comprobar(intentos,numero,entrada):
             if intentos == 1:
                 resultado = "perdiste "
                 print(resultado)
-                return [intentos, resultado]
+                intent = intentos+10
+                return [len(numero),intent, resultado]
             else:
                 for i in range(len(entrada)):
                     if numero[i] == entrada[i]:
@@ -43,7 +44,8 @@ def comprobar(intentos,numero,entrada):
                 if fijas == len(entrada):
                     resultado = "ganaste"
                     print(resultado)
-                    return [intentos, resultado]
+                    intent = 11 - intentos
+                    return [len(numero),intent, resultado]
                 intentos= intentos-1
                 print("tienes ", fijas, "fijas y tienes ", picas, "picas\n intentos restantes: ",intentos)
                 while True:
@@ -63,14 +65,13 @@ cifras = 0
 intentos = 0
 
 while True:
-    opcion = input("Este es un juego de picas y fijas, con cuantas cifras desea jugar:\n"
+    opcion = input("Este es un juego de picas y fijas, con cuantas cifras desea jugar:\n" #Menu de opciones
     "a.3 cifras(10 intentos)\nb.4 cifras(15 intentos)\nc.5 cifras(20 intentos)\n")
     if opcion == "a":
-        print("opcion de 3 cifras")
+        print("Opción de 3 cifras")
         cifras = 3
         intentos = 10
         s = generar_secreto(cifras)
-        print(s)
         while True:
             n = [int(x) for x in input("Ingrese un numero: ")]
             if validar_longitud(n,cifras) == True:
@@ -83,18 +84,15 @@ while True:
                 print("Ingrese un numero valido")
                 continue
 
-        print(n)
         final = comprobar(intentos, s, n)
-        print(final)
         nombre = input("Ingrese su nombre: ")
         final.append(nombre)
         print(final)
 
     elif opcion == "b":
-        print("opcion de 4 cifras")
+        print("Opción de 4 cifras")
         cifras = 4
         s = generar_secreto(cifras)
-        print(s)
         while True:
             n = [int(x) for x in input("Ingrese un numero: ")]
             if validar_longitud(n,cifras) == True:
@@ -107,19 +105,16 @@ while True:
                 print("Ingrese un numero valido")
                 continue
 
-        print(n)
         final = comprobar(intentos, s, n)
-        print(final)
         nombre = input("Ingrese su nombre: ")
         final.append(nombre)
         print(final)
 
     elif opcion == "c":
-        print("opcion de 5 cifras")
+        print("Opción de 5 cifras")
         cifras = 5
         intentos = 20
         s = generar_secreto(cifras)
-        print(s)
         while True:
             n = [int(x) for x in input("Ingrese un numero: ")]
             if validar_longitud(n,cifras) == True:
@@ -132,12 +127,12 @@ while True:
                 print("Ingrese un numero valido")
                 continue
 
-        print(n)
         final = comprobar(intentos, s, n)
-        print(final)
         nombre = input("Ingrese su nombre: ")
         final.append(nombre)
         print(final)
     else:
-        print("opcion invalida")
+        print("Fin del juego")
         break
+
+    #Revisar archivo para el ganador de cada categoria
